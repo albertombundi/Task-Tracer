1f0e42053b0aeedcbbad5e9b45a2769a84063350
albertombundi <albertombundi@outlook.com>
2025-09-19 17:13:21 +0100

    Component Button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Header } from './components/header/header';
 @Component({
   selector: 'app-root',
+  imports: [ Header, ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  title: string = 'Task Tracker';
+  
 }
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Button } from '../button/button';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ Button],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
-
+  title: string = 'Task Tracker';
 }
