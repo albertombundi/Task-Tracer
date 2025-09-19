9711a98a58c391171b00fdf91035768965f67c7d
albertombundi <albertombundi@outlook.com>
2025-09-19 19:59:53 +0100

    Component Input Data

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgStyle } from '@angular/common';
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [ NgStyle ],
   templateUrl: './button.html',
   styleUrl: './button.css'
 })
 export class Button {
+  @Input() text!: string;
+  @Input() color!: string;
+  @Output() btnClick = new EventEmitter();
+
+  onClick() {
+    this.btnClick.emit();
+  }
 
 }
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Button } from '../button/button';
 @Component({
   selector: 'app-header',
-  imports: [ Button],
+  imports: [ Button ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
   title: string = 'Task Tracker';
+  toggleAddTask() {
+    console.log('Toggle');
+  }
 }
