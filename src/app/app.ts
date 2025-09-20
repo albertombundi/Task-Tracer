cc755b515da876422ea8e1f0be6392d128f2dac1
albertombundi <albertombundi@outlook.com>
2025-09-20 02:41:37 +0100

    json-server Setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import { Header } from './components/header/header';
 import { Tasks } from './components/tasks/tasks';
 import { FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
+
 @Component({
   selector: 'app-root',
   imports: [ Header, Tasks, FontAwesomeModule ],
--- src/app/services/task-service.ts
+++ src/app/services/task-service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
@@ -7,7 +8,7 @@ import { TASKS } from '../mock-tasks';
   providedIn: 'root'
 })
 export class TaskService {
-  
+
   getTasks(): Observable<Task[]> {
     const tasks  = of(TASKS);
     return tasks;
