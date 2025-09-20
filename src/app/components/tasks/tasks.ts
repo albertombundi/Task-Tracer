5c8ef313e5d0018324941ba44e0f83d01eca6706
albertombundi <albertombundi@outlook.com>
2025-09-20 02:15:25 +0100

    creating task service, creating and subscribe to Observable

--- src/app/components/tasks/tasks.ts
+++ src/app/components/tasks/tasks.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TaskService } from '../../services/task-service';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
 import { TaskItem } from '../task-item/task-item';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule , TaskItem],
+  imports: [CommonModule, TaskItem],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
-export class Tasks {
-  tasks: Task[] = TASKS;
+export class Tasks implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks=tasks )
+  }
 }
